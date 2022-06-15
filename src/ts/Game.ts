93cf6e05547dab5090974a2cb0013e364a6a91ed
cnijssen <89836865+cnijssen@users.noreply.github.com>
2022-06-15 10:11:03 +0200

    Update Game.ts

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -1,16 +1,3 @@
-import { Level1 } from "./level1"
-import menuClick from "url:../sound/menu.mp3";
-import menuScroll from "url:../sound/menuscroll.mp3";
-import backgroundSong from "url:../sound/song.mp3";
-import moonImage from "../images/moon.png";
-
-
-
-let menuSong = new Audio(backgroundSong);
-let menuSound = new Audio(menuClick);
-let menuScrolling = new Audio(menuScroll);
-menuSong.volume = 0.4;
-menuScrolling.volume = 1;
 
 let body = document.body;
 let background = new Image;
@@ -23,22 +10,22 @@ logo.classList.add('logo');
 logo.src = moonImage;
 body.appendChild(logo);
 
-let title = document.createElement('h1');
-title.innerHTML = "Leren Spellen";
-title.classList.add('title');
-body.appendChild(title);
-
-
+// let title = document.createElement('h1');
+// title.innerHTML = "Leren Spellen";
+// title.classList.add('title');
+// body.appendChild(title);
 
 
 let buttonclass = document.createElement('div');
 buttonclass.classList.add('buttonclass');
 body.appendChild(buttonclass)
 
+
 let button = document.createElement('button')
 button.innerHTML = "Start game";
 button.classList.add('button');
 buttonclass.appendChild(button);
+
 
 let button2 = document.createElement('button')
 button2.innerHTML = "Tutorial";
@@ -60,73 +47,61 @@ let audioMute = document.createElement('div');
 audioMute.classList.add("audio");
 body.appendChild(audioMute);
 
-let buttonClick= 0
+let buttonClick = 0
 
-    menuSong.play();
-let audioclick = 0
-audioMute.addEventListener('click', function(){
-   if(audioclick>= 1){
- menuSong.play();
- audioclick--
- audioMute.classList.remove("audiomute");
-
-   } else{
-    menuSong.pause();
-    audioclick++
-    audioMute.classList.add("audiomute");
- }
-})
-
-buttonclass.addEventListener('click', function(){
-    if(buttonClick >= 1){
-buttonClick = 0
-    } 
-    
-    else {
-
- new Level1()
-
-
-button.classList.add('not-active')
-button2.classList.add('not-active')
-button3.classList.add('not-active')
-background.classList.add('not-active')
-title.classList.add('not-active')
-logo.classList.add('not-active')
-buttonClick++
-menuSound.play();
-menuSong.pause();
-exitButton.classList.remove("not-active");
-
-exitButton.classList.add("active");
-
- }
-})
-
-buttonclass.addEventListener('mouseover', function(){
-    menuScrolling.play(); 
-
-})
-
-exitButton.addEventListener('click', function(){
-   
-    buttonClick = 0
-button.classList.remove('not-active')
-button2.classList.remove('not-active')
-button3.classList.remove('not-active')
-background.classList.remove('not-active')
-title.classList.remove('not-active')
-logo.classList.remove('not-active')
 menuSong.play();
-exitButton.classList.add("not-active");
+let audioclick = 0
+audioMute.addEventListener('click', function () {
+    if (audioclick >= 1) {
+        menuSong.play();
+        audioclick--
+        audioMute.classList.remove("audiomute");
+
+    } else {
+        menuSong.pause();
+        audioclick++
+        audioMute.classList.add("audiomute");
+    }
 })
- 
 
+buttonclass.addEventListener('click', function () {
+    if (buttonClick >= 1) {
+        buttonClick = 0
+    }
 
+    else {
+        window.close()
+        window.open("game.html")
 
+        button.classList.add('not-active')
+        button2.classList.add('not-active')
+        button3.classList.add('not-active')
+        background.classList.add('not-active')
+        title.classList.add('not-active')
+        logo.classList.add('not-active')
+        buttonClick++
+        menuSound.play();
+        menuSong.pause();
+        exitButton.classList.remove("not-active");
 
+        exitButton.classList.add("active");
 
+    }
+})
 
+buttonclass.addEventListener('mouseover', function () {
+    menuScrolling.play();
 
+})
 
-
+exitButton.addEventListener('click', function () {
+    buttonClick = 0
+    button.classList.remove('not-active')
+    button2.classList.remove('not-active')
+    button3.classList.remove('not-active')
+    background.classList.remove('not-active')
+    title.classList.remove('not-active')
+    logo.classList.remove('not-active')
+    menuSong.play();
+    exitButton.classList.add("not-active");
+})
