78db19d48914c937d5c811ac48a826eaa3982709
Camryn Terlouw <86618337+cter079@users.noreply.github.com>
2022-06-19 22:27:43 +0200

    Update Letter.ts

--- src/ts/Letter.ts
+++ src/ts/Letter.ts
@@ -4,11 +4,11 @@ import { Level1 } from "./Level1"
 
 export class Letter extends PIXI.Sprite {
 
-    rigidBody: Matter.Body
-    coinSound:HTMLAudioElement
-    game:Level1
-    
-    constructor(xpos:number, ypos:number, texture: PIXI.Texture, game: Level1, sound:HTMLAudioElement) {
+    public rigidBody: Matter.Body
+    private coinSound: HTMLAudioElement
+    public game: Level1
+
+    constructor(xpos: number, ypos: number, texture: PIXI.Texture, game: Level1, sound: HTMLAudioElement) {
         super(texture)
         this.game = game
 
@@ -17,23 +17,23 @@ export class Letter extends PIXI.Sprite {
         this.rigidBody = Matter.Bodies.circle(xpos, ypos, 10, { friction: 0.00001, restitution: 0, density: 0.001, label: "Coin" }) //x,y,radius
         Matter.Composite.add(game.engine.world, this.rigidBody)
         this.rigidBody.isStatic = true
-        
+
         this.coinSound = sound
     }
 
-    update() {
+    public update() {
         this.position.set(this.rigidBody.position.x, this.rigidBody.position.y)
         this.rotation = this.rigidBody.angle
 
     }
 
-    resetPosition() {
-        Matter.Body.setPosition(this.rigidBody, {x:120, y:30})
-        Matter.Body.setVelocity(this.rigidBody, {x:0, y:0})
+    public resetPosition() {
+        Matter.Body.setPosition(this.rigidBody, { x: 120, y: 30 })
+        Matter.Body.setVelocity(this.rigidBody, { x: 0, y: 0 })
         Matter.Body.setAngularVelocity(this.rigidBody, 0)
     }
 
-    beforeUnload() {
+    public beforeUnload() {
         this.coinSound.play()
     }
 }
