3fec4d35b6f39d76e59e96b29b9d0a6e2d71913d
Camryn Terlouw <86618337+cter079@users.noreply.github.com>
2022-06-19 22:28:27 +0200

    Update ui.ts

--- src/ts/ui.ts
+++ src/ts/ui.ts
@@ -5,73 +5,79 @@ import { Player } from "./Player"
 
 export class UI extends PIXI.Container {
     public rigidBody: Matter.Body
-    timeLeft:PIXI.Text
-    score:string = ""
-    xspeed = 0
-    yspeed = 0
-    speed: number = 0
-    game: Level1
-    pixi:PIXI.Application
-    player: Player
-    time:number
+    private timeLeft: PIXI.Text
+    private finish: PIXI.Text
+    private score: string = ""
+    private xspeed = 0
+    private yspeed = 0
+    private speed: number = 0
+    public game: Level1
+    public pixi: PIXI.Application
+    public player: Player
+    private time: number
+    private time2: string
 
     constructor(game: Level1) {
         super()
         this.game = game
-        this.x = game.pixi.screen.width/2
-        this.y = game.pixi.screen.height/2
-        
+        this.x = game.pixi.screen.width / 2
+        this.y = game.pixi.screen.height / 2
+
         const style = new PIXI.TextStyle({
             fontFamily: 'ArcadeFont',
             fontSize: 40,
             fontWeight: 'bold',
             fill: ['#FFFFFF']
         })
-       
+
 
 
         window.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e))
-        this.timeLeft = new PIXI.Text(`tijd over: ${this.time}`, style)
+        this.timeLeft = new PIXI.Text(`tijd over: ${this.time2}`, style)
         this.addChild(this.timeLeft)
-       
+        this.finish = new PIXI.Text(`finish!`, style)
+        this.addChild(this.finish)
+        this.finish.x = 4000
+        this.finish.y = 110
+
+
         this.time = 100
     }
 
-    update(){
+    public update() {
         let mapwidth = 5000
         let mapheight = 500
         this.x = this.clamp(this.x + this.xspeed, 0, mapwidth)
         this.y = this.clamp(this.y + this.yspeed, 0, mapheight)
 
-        this.time -= 0.016
-       this.timeLeft.text = `tijd over: ${this.time}`
-        }
+        this.time -= 0.0083
+        this.time2 = this.time.toFixed(0)
+        this.timeLeft.text = `tijd over: ${this.time2}`
+    }
 
-    
 
-       
+
+
 
     private clamp(num: number, min: number, max: number) {
         return Math.min(Math.max(num, min), max)
     }
-   
-    
 
-   private  onKeyDown(e: KeyboardEvent) {
+
+
+    private onKeyDown(e: KeyboardEvent) {
         if (e.key === " " || e.key === "ArrowUp") {
-            
+
         }
         switch (e.key) {
             case "ArrowLeft":
-                this.x -= 5
                 break
             case "ArrowRight":
-                this.x += 5
                 break
         }
     }
 
-   
+
 
 
 
@@ -82,4 +88,3 @@ export class UI extends PIXI.Container {
 
 
 
-   
